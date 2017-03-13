62b51fa815071472e1ca4f0a911262727fd22ea4
Dubilowicz <pawel.dubilowicz@programisci.eu>
2017-03-13 13:25:58 +0100

    Initial commit

---
+++ src/main/java/eu/programisci/FakturerApplication.java
@@ -0,0 +1,12 @@
+package eu.programisci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturerApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/programisci/FakturerApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.programisci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FakturerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
